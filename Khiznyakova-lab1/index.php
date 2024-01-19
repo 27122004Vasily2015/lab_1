a2575ef1dcedb5963c50238bc40ffc82fb363ceb
27122004Vasily2015 <101931202+27122004Vasily2015@users.noreply.github.com>
2024-01-20 00:48:49 +0500

    Лабораторная работа №1
    Лабораторная работа №1 все файлы

---
+++ Khiznyakova-lab1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="name">
+        <h3>Тренировочные задания по ЕГЭ</h3>
+        <h1>Информатика</h1>
+        <h4>Тольяттинский государственный университет</h4>
+    </div>
+    <div class="sign-in">
+        <h4>Вы вошли на сайт как гость</h4>
+        <button class="login">Войти</button>
+        <button class="registr">Регистрация</button>
+    </div>
+    </header>
+
+    <main>
+        <div class="tasks">  
+            <h2><a href="/task.php?task=0">1 вариант задания - системы счисления</a></h2>
+            <h2><a href="/task.php?task=1">2 вариант задания - Объем памяти</a></h2>
+            <h2><a href="/task.php?task=2">3 вариант задания - подсчет количества вхождений рандомного символа</a></h2>
+        </div>
+    </main>
+</body>
+
+</html>
